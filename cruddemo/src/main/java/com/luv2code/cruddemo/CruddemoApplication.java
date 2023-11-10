bf0aa38ba09cf7a44165fca88671d0f0a69c2cd7
Meet Shah <meetshah7199@gmail.com>
2023-11-09 20:10:46 -0500

    Creating DAO and Mapping it with Entity Manager.

--- cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,13 +1,36 @@
 package com.luv2code.cruddemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
+
+@SpringBootApplication 
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+		
+		return runner->{createStudent(studentDAO);};
+	}
+
+	private void createStudent(StudentDAO studentDAO){
+		//create the student object
+		System.out.println("Creating new student Object....");
+		Student tempStudent = new Student("Meet", "Shah", "meetshah.sr@gmail.com");
+		
+		//save the student object
+		System.out.println("Saving the student....");
+		studentDAO.save(tempStudent);
+		
+		//display id of the saved student
+		System.out.println("New student created with ID: "+tempStudent.getId());
+	}
 }
---
+++ cruddemo/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,31 @@
+package com.luv2code.cruddemo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.luv2code.cruddemo.entity.Student;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // Define field for entity manager
+    private EntityManager entitManager;
+
+    // Define entity manager using constructor injection
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entitManager) {
+        this.entitManager = entitManager;
+    }
+
+    //Define Save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+      entitManager.persist(theStudent);  
+    }
+    
+}
